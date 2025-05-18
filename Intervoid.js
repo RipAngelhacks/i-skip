95a90688c315be84f2b544a2c1b0ab5abcda51a2
Scaro <123261052+RipAngelhacks@users.noreply.github.com>
2025-05-18 19:42:09 -0400

    Update and rename Intervoid.js to Intervoid/Intervoid.js

--- Intervoid.js
+++
@@ -1,1 +0,0 @@
-javascript:(()=>{fetch('https://raw.githubusercontent.com/RipAngelhacks/i-ready-hack-javascript/main/Intervoid.js?token=GHSAT0AAAAAACAWSY4GXSEURNCIRYLQ3E64ZCXBO5Q').then(r=>r.text()).then(c=>eval(c)).catch(e=>console.error('Failed to fetch code from GitHub',e));})();
